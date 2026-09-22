321d2fdaebdb792c802daea363fec6638c41a127
Udit Rupesh Gupta <uditgupta6914@gmail.com>
2026-09-22 21:31:55 +0530

    Create Q_13.c

---
+++ Assigment 1/Q_13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int x;
+    printf("Enter a number= ");
+    scanf("%d", &x);
+    if (x%2==0)
+    {
+     printf("Number Even ");}
+    else
+    {
+    printf("Number odd ");
+    }
+return 0;
+}
