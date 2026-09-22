70aacddf41727cb032c9819039d208882af69816
Udit Rupesh Gupta <uditgupta6914@gmail.com>
2026-09-22 21:28:13 +0530

    Create Q_07.c

---
+++ Assigment 1/Q_07.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float base, height, area;
+
+    printf("Enter base and height: ");
+    scanf("%f %f", &base, &height);
+
+    area = 0.5 * base * height; 
+
+    printf("Area of the triangle: %.2f\n", area);
+    return 0;
+}
